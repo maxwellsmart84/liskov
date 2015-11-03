86a015dec17e40702e6563fa8a4f9f9d3cad148f
Maxwell Krause <maxwell.l.krause@gmail.com>
2015-11-03 18:10:23 -0500

    initial commit on all changes, added onclick function with login page, added login page, APPLIED HIDDEN CLASS TO CHATBOX AND SIDENAV, completed AJAX put with constructor data

--- js/constructors.js
+++ js/constructors.js
@@ -1,5 +1,5 @@
 //// user ////
-var user = function(options){
+var User = function(options){  //MADE THIS A CAPITAL SINCE ITS A CLASSNAME
   this.username = options.username;
   this.email = options.email;
   this.avatar = options.avatar;
--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 
 var page = {
 
+  url: "http://tiny-tiny.herokuapp.com/collections/chatorex",
+
   init: function () {
     page.initStyling();
     page.initEvents();
@@ -14,8 +16,37 @@ var page = {
   },
 
   initEvents: function () {
+    page.loginSub();
 
-  }
+  },
+  loginSub: function(){
+    $("#loginContainer").on("click", "#loginSubmit", function(event){
+      event.preventDefault;
+      var $submitBtn = $(this);//WASNT SURE IF I NEEDED THIS
+      $(".col-md-8[rel='chatbox']").removeClass(hidden-class); //REMOVES ALL HIDDEN CLASSES FROM CHATBOX
+      $(".col-md-4[rel='sidebar']").removeClass(hidden-class);
+      $(".col.md-8[rel='login']").addClass(hidden-class);
+      var userName = $("input[name='username']").val("");//USER INPUT COLLECTION STRINGIFIED
+      var userEmail = $("input[name='email']").val("");
+      var userAvatar = $("input[name='avatar']").val("");
+      userLoginAdd = new User { //ADDING DATA TO CONSTRUCTOR
+        username: userName;
+        email: userEmail;
+        avatar: userAvatar;
+        $.ajax({     //AJAX PUSH TO SERVER
+          url: page.url,
+          method:"POST",
+          data: userLoginAdd,
+          success: function (data){
+            console.log("SUCCESS!", data);
+          },
+          failure: function (data) {
+            console.log("FAILURE!!!");
+          }
+        });
+      };
+    });
+  },
 
 
 };
