1b792d23d83039b9406d2d718691a6d7e2f19185
Maxwell Krause <maxwell.l.krause@gmail.com>
2015-11-03 18:46:51 -0500

    changing to adjust to grady's merge

--- js/main.js
+++ js/main.js
@@ -20,12 +20,12 @@ var page = {
 
   },
   loginSub: function(){
-    $("#loginContainer").on("click", "#loginSubmit", function(event){
+    $(".container").on("click", "#loginSubmit", function(event){
       event.preventDefault;
       var $submitBtn = $(this);//WASNT SURE IF I NEEDED THIS
-      $(".col-md-8[rel='chatbox']").removeClass(hidden-class); //REMOVES ALL HIDDEN CLASSES FROM CHATBOX
-      $(".col-md-4[rel='sidebar']").removeClass(hidden-class);
-      $(".col.md-8[rel='login']").addClass(hidden-class);
+      $(".col-md-8").removeClass(hidden-class); //REMOVES ALL HIDDEN CLASSES FROM CHATBOX
+      $(".col-md-4").removeClass(hidden-class);
+      $(".col.md-12").addClass(hidden-class);
       var userName = $("input[name='username']").val("");//USER INPUT COLLECTION STRINGIFIED
       var userEmail = $("input[name='email']").val("");
       var userAvatar = $("input[name='avatar']").val("");
--- templates.js
+++
@@ -1,9 +0,0 @@
-var templates = {
-  sideBarUser: [
-    "<li><img src='<%= avatar %>' /><h5><%= username %></h5></li>"
-  ].join(""),
-  message: [
-    "<li><img src='<%= avatar %>' /><h3><%= username %></h3>",
-    "<p><%= message %></p></li>"
-  ].join("")
-};
