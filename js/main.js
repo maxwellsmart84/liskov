9eaf4af6f42117a5f9cc7418d93b29eba40e721f
Alison Arsenault <AlisonHorta@Alisons-MacBook-Pro.local>
2015-11-03 19:07:32 -0500

    Template added to HTML temporarily, adding message submit functions:

--- js/main.js
+++ js/main.js
@@ -16,7 +16,34 @@ var page = {
   },
 
   initEvents: function () {
+    var messageData = []
 
+    $('form').on('submit', function(event) {
+      event.preventDefault();
+      var newMessage = {
+        avatar: "http://www.fillmurray.com/200/300",
+        username: "Bill Murray",
+        content: $('textarea').val(),
+      };
+      messageData.push(newMessage);
+      page.loadTemplate($('.col-md-8'), newMessage, $('#test').html());
+      $('textarea').val('');
+    });
+
+    $('li').on('click', 'button', function () {
+
+    })
+
+  },
+
+  loadTemplate: function ($el, data, tmpl) {
+    var template = _.template(tmpl);
+    var html = template(data);
+    $el.prepend(html);
+  },
+
+  deleteMessage: function () {
+    
   }
 
 
